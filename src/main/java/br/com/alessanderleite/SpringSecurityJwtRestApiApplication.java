d4a73ec17582f81cca6792dc7a58ec30b0d1059d
alessanderleite <alessander.logistica@gmail.com>
2019-09-14 20:57:05 -0300

    initial commit

---
+++ src/main/java/br/com/alessanderleite/SpringSecurityJwtRestApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.alessanderleite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwtRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJwtRestApiApplication.class, args);
+	}
+
+}
