6a954a48fd6bc813aaca6dd3609db2737f0d321a
alessanderleite <alessander.logistica@gmail.com>
2019-10-06 11:52:28 -0300

    add the @Transational annotation

--- src/main/java/br/com/alessanderleite/repositories/FuncionarioRepository.java
+++ src/main/java/br/com/alessanderleite/repositories/FuncionarioRepository.java
@@ -1,9 +1,11 @@
 package br.com.alessanderleite.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.alessanderleite.entities.Funcionario;
 
+@Transactional(readOnly = true)
 public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
 
 	Funcionario findByCpf(String cpf);
