1aaa7cc6f85f96ba35991ffc6d1898c3332f4527
alessanderleite <alessander.logistica@gmail.com>
2019-10-09 21:48:43 -0300

    create the class EmpresaServiceImpl

---
+++ src/main/java/br/com/alessanderleite/services/impl/EmpresaServiceImpl.java
@@ -0,0 +1,32 @@
+package br.com.alessanderleite.services.impl;
+
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import br.com.alessanderleite.entities.Empresa;
+import br.com.alessanderleite.repositories.EmpresaRepository;
+import br.com.alessanderleite.services.EmpresaService;
+
+@Service
+public class EmpresaServiceImpl implements EmpresaService {
+
+	private static final Logger log = LoggerFactory.getLogger(EmpresaServiceImpl.class);
+	
+	private EmpresaRepository empresaRepository;
+	
+	@Override
+	public Optional<Empresa> buscarPorCnpj(String cnpj) {
+		log.info("Buscando uma empresa para o CNPJ {}", cnpj);
+		return Optional.ofNullable(empresaRepository.findByCnpj(cnpj));
+	}
+
+	@Override
+	public Empresa persistir(Empresa empresa) {
+		log.info("Persistindo empresa: {}", empresa);
+		return this.empresaRepository.save(empresa);
+	}
+
+}
