e551a722353a0955d204e09187cfb0f4fd971c79
alessanderleite <alessander.logistica@gmail.com>
2019-10-12 16:28:27 -0300

    add the @EnableCaching annotation

--- src/main/java/br/com/alessanderleite/SpringSecurityJwtRestApiApplication.java
+++ src/main/java/br/com/alessanderleite/SpringSecurityJwtRestApiApplication.java
@@ -2,8 +2,10 @@ package br.com.alessanderleite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class SpringSecurityJwtRestApiApplication {
 
 	public static void main(String[] args) {
