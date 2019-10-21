399def42798fe91ccf3a97d6f2813506acc929c1
alessanderleite <alessander.logistica@gmail.com>
2019-10-20 21:58:31 -0300

    create the class LancamentoRepositoryTest

---
+++ src/test/java/br/com/alessanderleite/repositories/LancamentoRepositoryTest.java
@@ -0,0 +1,47 @@
+package br.com.alessanderleite.repositories;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import br.com.alessanderleite.entities.Empresa;
+import br.com.alessanderleite.entities.Funcionario;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class LancamentoRepositoryTest {
+	
+	@Autowired
+	private LancamentoRepository lancamentoRepository;
+	
+	@Autowired
+	private FuncionarioRepository funcionarioRepository;
+	
+	@Autowired
+	private EmpresaRepository empresaRepository;
+	
+	private Long funcionarioId;
+	
+	@Before
+	public void setUp() throws Exception {
+		Empresa empresa = this.empresaRepository.save(obterDadosEmpresa());
+		
+		Funcionario funcionario = this.funcionarioRepository.save(obterDadosFuncionario(empresa));
+	}
+
+	private Funcionario obterDadosFuncionario(Empresa empresa) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private Empresa obterDadosEmpresa() {
+		Empresa empresa = new Empresa();
+		empresa.setRazaoSocial("Empresa de exemplo");
+		empresa.setCnpj("51463645000100");
+		return empresa;
+	}
+}
