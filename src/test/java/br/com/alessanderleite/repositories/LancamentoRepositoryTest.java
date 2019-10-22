546d339a5f4fc41adedea2a0be5177fcf1f63343
alessanderleite <alessander.logistica@gmail.com>
2019-10-21 21:50:30 -0300

    update the LancamentoRepositoryTest to run the unit test

--- src/test/java/br/com/alessanderleite/repositories/LancamentoRepositoryTest.java
+++ src/test/java/br/com/alessanderleite/repositories/LancamentoRepositoryTest.java
@@ -1,14 +1,28 @@
 package br.com.alessanderleite.repositories;
 
+import static org.junit.Assert.assertEquals;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import br.com.alessanderleite.entities.Empresa;
 import br.com.alessanderleite.entities.Funcionario;
+import br.com.alessanderleite.entities.Lancamento;
+import br.com.alessanderleite.enums.PerfilEnum;
+import br.com.alessanderleite.enums.TipoEnum;
+import br.com.alessanderleite.utils.PasswordUtils;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -31,11 +45,50 @@ public class LancamentoRepositoryTest {
 		Empresa empresa = this.empresaRepository.save(obterDadosEmpresa());
 		
 		Funcionario funcionario = this.funcionarioRepository.save(obterDadosFuncionario(empresa));
+		this.funcionarioId = funcionario.getId();
+		
+		this.lancamentoRepository.save(obterDadosLancamentos(funcionario));
+		this.lancamentoRepository.save(obterDadosLancamentos(funcionario));
+		
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		this.empresaRepository.deleteAll();
+	}
+	
+	@Test
+	public void testBuscarLancamentosPorFuncionarioId() {
+		List<Lancamento> lancamentos = this.lancamentoRepository.findByFuncionarioId(funcionarioId);
+		
+		assertEquals(2, lancamentos.size());
+	}
+	
+	@Test
+	public void testBuscarLancamentosPorFuncionarioIdPaginado() {
+		PageRequest page = new PageRequest(0, 10);
+		Page<Lancamento> lancamentos = this.lancamentoRepository.findByFuncionarioId(funcionarioId, page);
+		
+		assertEquals(2, lancamentos.getTotalElements());
+	}
+	
+	private Lancamento obterDadosLancamentos(Funcionario funcionario) {
+		Lancamento lancamento = new Lancamento();
+		lancamento.setData(new Date());
+		lancamento.setTipo(TipoEnum.INICIO_ALMOCO);
+		lancamento.setFuncionario(funcionario);
+		return lancamento;
 	}
 
-	private Funcionario obterDadosFuncionario(Empresa empresa) {
-		// TODO Auto-generated method stub
-		return null;
+	private Funcionario obterDadosFuncionario(Empresa empresa) throws NoSuchAlgorithmException {
+		Funcionario funcionario = new Funcionario();
+		funcionario.setNome("Fulano de Tal");
+		funcionario.setPerfil(PerfilEnum.ROLE_USUARIO);
+		funcionario.setSenha(PasswordUtils.gerarBCrypt("123456"));
+		funcionario.setCpf("24291173474");
+		funcionario.setEmail("email@email.com");
+		funcionario.setEmpresa(empresa);
+		return funcionario;
 	}
 
 	private Empresa obterDadosEmpresa() {
