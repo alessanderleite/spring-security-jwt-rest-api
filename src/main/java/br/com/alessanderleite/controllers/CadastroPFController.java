592c159c1da0ad8e9e0797e158659c4fc627c7c2
alessanderleite <alessander.logistica@gmail.com>
2019-10-27 15:07:47 -0300

    create the class named CadastroPFController

---
+++ src/main/java/br/com/alessanderleite/controllers/CadastroPFController.java
@@ -0,0 +1,52 @@
+package br.com.alessanderleite.controllers;
+
+import java.security.NoSuchAlgorithmException;
+
+import javax.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.alessanderleite.dtos.CadastroPFDto;
+import br.com.alessanderleite.response.Response;
+import br.com.alessanderleite.services.EmpresaService;
+import br.com.alessanderleite.services.FuncionarioService;
+
+@RestController
+@RequestMapping("/api/cadastrar-pf")
+@CrossOrigin(origins = "*")
+public class CadastroPFController {
+
+	private static Logger log = LoggerFactory.getLogger(CadastroPFController.class);
+	
+	@Autowired
+	private EmpresaService empresaService;
+	
+	@Autowired
+	private FuncionarioService funcionarioService;
+	
+	public CadastroPFController() {}
+	
+	/**
+	 * Cadastra um funcionário pessoa física no sistema.
+	 * 
+	 * @param cadastroPFDto
+	 * @param result
+	 * @return ResponseEntity<Response<CadastroPFDto>>
+	 * @throws NoSuchAlgorithmException
+	 */
+	public ResponseEntity<Response<CadastroPFDto>> cadastrar(@Valid @RequestBody CadastroPFDto cadastroPFDto,
+			BindingResult result) throws NoSuchAlgorithmException {
+		log.info("Cadastrando PF: {}", cadastroPFDto.toString());
+		Response<CadastroPFDto> response = new Response<CadastroPFDto>();
+		
+		return null;
+	}
+}
