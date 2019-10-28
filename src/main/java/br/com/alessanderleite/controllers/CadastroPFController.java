af0ae8b3e1243d5a98bde2d25d9cffa3993e6ebd
alessanderleite <alessander.logistica@gmail.com>
2019-10-27 22:22:20 -0300

    create the method validarDadosExistentes()

--- src/main/java/br/com/alessanderleite/controllers/CadastroPFController.java
+++ src/main/java/br/com/alessanderleite/controllers/CadastroPFController.java
@@ -1,6 +1,7 @@
 package br.com.alessanderleite.controllers;
 
 import java.security.NoSuchAlgorithmException;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -9,12 +10,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alessanderleite.dtos.CadastroPFDto;
+import br.com.alessanderleite.entities.Empresa;
 import br.com.alessanderleite.response.Response;
 import br.com.alessanderleite.services.EmpresaService;
 import br.com.alessanderleite.services.FuncionarioService;
@@ -42,11 +46,33 @@ public class CadastroPFController {
 	 * @return ResponseEntity<Response<CadastroPFDto>>
 	 * @throws NoSuchAlgorithmException
 	 */
+	@PostMapping
 	public ResponseEntity<Response<CadastroPFDto>> cadastrar(@Valid @RequestBody CadastroPFDto cadastroPFDto,
 			BindingResult result) throws NoSuchAlgorithmException {
 		log.info("Cadastrando PF: {}", cadastroPFDto.toString());
 		Response<CadastroPFDto> response = new Response<CadastroPFDto>();
 		
+		validarDadosExistentes(cadastroPFDto, result);
 		return null;
+	}
+
+	/**
+	 * Verifica se a empresa está cadastrada e se o funcionário não existe na base de dados.
+	 * 
+	 * @param cadastroPFDto
+	 * @param result
+	 */
+	private void validarDadosExistentes(@Valid CadastroPFDto cadastroPFDto, BindingResult result) {
+		Optional<Empresa> empresa = this.empresaService.buscarPorCnpj(cadastroPFDto.getCnpj());
+		if (!empresa.isPresent()) {
+			result.addError(new ObjectError("empresa", "Empresa não cadastrada"));
+		}
+		
+		this.funcionarioService.buscarPorCpf(cadastroPFDto.getCpf())
+				.ifPresent(func -> result.addError(new ObjectError("funcionario", "CPF já existente.")));
+		
+		this.funcionarioService.buscarPorEmail(cadastroPFDto.getEmail())
+				.ifPresent(func -> result.addError(new ObjectError("funcionario", "Email já existente.")));
+		
 	}
 }
