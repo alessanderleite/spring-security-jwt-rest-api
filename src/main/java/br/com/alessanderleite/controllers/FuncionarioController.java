ac606da6ecd1b008452035512e9bc95adc10fed3
alessanderleite <alessander.logistica@gmail.com>
2019-10-29 22:37:43 -0300

    create the new class named FuncionarioController

---
+++ src/main/java/br/com/alessanderleite/controllers/FuncionarioController.java
@@ -0,0 +1,25 @@
+package br.com.alessanderleite.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.alessanderleite.services.FuncionarioService;
+
+@RestController
+@RequestMapping("/api/funcionarios")
+@CrossOrigin(origins = "*")
+public class FuncionarioController {
+
+	private static final Logger log = LoggerFactory.getLogger(FuncionarioController.class);
+	
+	@Autowired
+	private FuncionarioService funcionarioService;
+
+	public FuncionarioController() {}
+	
+	
+}
