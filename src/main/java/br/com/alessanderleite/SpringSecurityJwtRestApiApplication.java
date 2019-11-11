37701d27ff8c78807e0d67eb0d5a7af7b753a50f
alessanderleite <alessander.logistica@gmail.com>
2019-11-10 21:03:24 -0300

    disable SecurityAutoConfiguration.class

--- src/main/java/br/com/alessanderleite/SpringSecurityJwtRestApiApplication.java
+++ src/main/java/br/com/alessanderleite/SpringSecurityJwtRestApiApplication.java
@@ -2,10 +2,9 @@ package br.com.alessanderleite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication//(exclude = { SecurityAutoConfiguration.class })
 @EnableCaching
 public class SpringSecurityJwtRestApiApplication {
 
